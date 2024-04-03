0cc1519187b8b39a6284aabaa5c8ec71afcc2efc
JustinTorcato <158062051+JustinTorcato@users.noreply.github.com>
2024-04-03 20:52:15 +0100

    Complete restart
    Redoing assignment one to help with fundamentals

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -6,18 +6,15 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Die
+    class Die
     {
-        /*
-         * The Die class should contain one property to hold the current die value,
-         * and one method that rolls the die, returns and integer and takes no parameters.
-         */
+        public static void roll()
+        {
+            //https://youtu.be/1gSFd-YVFP8?si=Rpdr2gLzbcuNrAvP
 
-        //Property
-
-
-        //Method
-
-
+            Random random = new Random();
+            int num = random.Next(1, 7);
+            Console.WriteLine(num);
+        }
     }
 }
--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -10,7 +10,8 @@ namespace CMP1903_A1_2324
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Die die1 = new Die();
+            Die.roll();
             Console.ReadLine();
         }
     }
